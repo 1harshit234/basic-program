f8529b259bc36a13d131c43b66a4c03d1f83d4a3
Harshit Mishra <165307122+1mishraji21@users.noreply.github.com>
2024-05-09 00:16:30 -0700

    Update sum.py

--- sum.py
+++ sum.py
@@ -1,3 +1,6 @@
+#this is the basic programming in which the basic sum of the two number happen
 x = int(input("write the number you want1"))
 y = int(input("write the number you want2"))
+z = x+y
+print(z)
 print(x+y)
