d8224ebb222898645eba9c2cf719b916bd0c525b
Harshit Mishra <165307122+1mishraji21@users.noreply.github.com>
2024-05-09 03:47:01 -0700

    Add files via upload

---
+++ check.py
@@ -0,0 +1,7 @@
+x = int(input("write the number you want"))
+if x>0:
+    print("the given number is positive")
+elif x<0:
+    print("the given is negative")
+else:
+    print("the given number is zero")
