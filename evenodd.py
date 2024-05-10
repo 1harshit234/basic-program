94e8ca023e0c38198fad855d436fa72e5f7366d2
Harshit Mishra <165307122+1harshit234@users.noreply.github.com>
2024-05-09 20:24:19 -0700

    Add files via upload

---
+++ evenodd.py
@@ -0,0 +1,8 @@
+#taking the input from the user
+x = int(input("write the number which you want"))
+print(x)
+#comparing the number 
+if (x%2==0):
+    print("the given number is even")
+else:
+    print("the given number is odd")
