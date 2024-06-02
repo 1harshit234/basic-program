0c773540146cb7daf6562977434efc9a43ad4c1c
Harshit Mishra <165307122+1harshit234@users.noreply.github.com>
2024-06-01 20:36:16 -0700

    enumerate.py

--- enumerate.py
+++ enumerate.py
@@ -1,5 +1,20 @@
 
 
+
+ #method 1 here we have used tuple
+l = (10,20,30,40)
+for i,x in enumerate(l,start = 6):
+ print(i,x)
+
+
+#method 2 here we have used list
+l = [ 10,20,30,40]
+for i,x in enumerate(l,start = 6):
+ print(i,x)
+
+
+#method 3 here we have used strinng
 l = "harshit"
 for i,x in enumerate(l,start = 6):
  print(i,x)
+
